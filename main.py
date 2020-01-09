f11b299645e50b7bfe05dd70f8fe5688e110e13d
Wes Chow <wgesh8@gmail.com>
2020-01-09 22:57:51 +0000

    Update format of site, change variables to input name, add Firestore function

--- main.py
+++ main.py
@@ -1,26 +1,47 @@
-
-
 from flask import Flask, request, render_template
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+# Setup Firebase service account
+cred = credentials.Certificate("nwhacks2020test-firebase-adminsdk-ovv8c-68a0d92ab8.json")
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
 #Explaining Imports: Flask - Html support | requests - used to take text area | render_template - opening html files |
 
 app = Flask(__name__)
+
 #===Dummy Data===#
-output = ''
-
-
+first_name = ''
+last_name = ''
+return_text = ''
 
 
 @app.route('/') #creates a new webpage
 @app.route('/home') #stacked so multiurl links to same place
 def home():
-  return render_template('home.html') #render_templates renders html file in /templates
+    return render_template('home.html') #render_templates renders html file in /templates
 
 @app.route('/home', methods=['POST']) 
 def submit():
-    output = '{}'.format(request.form['textInput']) #grab text from textarea in html
-    output = output.upper() #processing could happen here
-    print(output)
-    return render_template('home.html', output=output) #this loads output into the html file. neat.
+    first_name = '{}'.format(request.form['textInput']) #grab text from textarea in html
+    last_name = '{}'.format(request.form['textInput2'])
+
+    # send to firebase
+    doc_ref = db.collection(u'users').document(first_name[0] + last_name)
+    doc_ref.set({
+        u'first': first_name,
+        u'last': last_name,
+    })
+
+    first_name = first_name.upper() #processing could happen here
+    last_name = last_name.upper()
+
+    print(first_name)
+    print(last_name)
+    return render_template('home.html', return_text=first_name + ' ' + last_name) #this loads first_name into the html file. neat.
 
 
 
