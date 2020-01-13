2ebb06c0daa12d6e97a818917a6070cc0c9d6507
Wes Chow <wgesh8@gmail.com>
2020-01-13 08:27:03 +0000

    Fix security issue, other

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, send_from_directory
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
 # Setup Firebase service account
-cred = credentials.Certificate("nwhacks2020test-firebase-adminsdk-ovv8c-68a0d92ab8.json")
+cred = credentials.Certificate("REMOVED KEY DUE TO SECURITY")
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
@@ -24,10 +24,15 @@ return_text = ''
 def home():
     return render_template('home.html') #render_templates renders html file in /templates
 
+
+@app.route('/css/<path>')
+def send_style(path):
+    return send_from_directory('css', path)
+
 @app.route('/home', methods=['POST']) 
 def submit():
     first_name = '{}'.format(request.form['textInput']) #grab text from textarea in html
-    last_name = '{}'.format(request.form['textInput2'])
+    last_name = '{}'.format(request.form['textInpu  t2'])
 
     # send to firebase
     doc_ref = db.collection(u'users').document(first_name[0] + last_name)
@@ -43,8 +48,9 @@ def submit():
     print(last_name)
     return render_template('home.html', return_text=first_name + ' ' + last_name) #this loads first_name into the html file. neat.
 
-
-
+@app.route('/box')
+def box():
+  return render_template('box.html')
 
 
 if __name__ == '__main__':
